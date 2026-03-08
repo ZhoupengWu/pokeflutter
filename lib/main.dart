5cabe0ca4caff08a05559fd01c0b70496c13b788
ZhoupengWu <182263202+ZhoupengWu@users.noreply.github.com>
2026-03-08 18:01:38 +0100

    Main page

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MainPage extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello world"),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(top: 76, left: 24, right: 24),
+            child: Text("Pokédex")),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Text("Use the advanced search to find Pokémon by type, weakness, ability and more!"))
+        ],
+      ),
     );
   }
 }
