82d8e0a2621c942dbc67e92ee571293d7b1b6f58
ZhoupengWu <182263202+ZhoupengWu@users.noreply.github.com>
2026-03-14 22:08:56 +0100

    Pokemon item view

--- lib/views/widgets/pokemon_list.dart
+++ lib/views/widgets/pokemon_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../model/pokemon_list_item.dart';
+import '../../views/widgets/grid_item.dart';
 
 class PokemonList extends StatelessWidget {
   final List<PokemonListItem> pokemonList;
@@ -23,10 +24,7 @@ class PokemonList extends StatelessWidget {
           mainAxisExtent: 100
         ),
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            decoration: BoxDecoration(color: Colors.blue),
-            child: Text(pokemonList[index].name)
-          );
+          return GridItem(pokemon: pokemonList[index]);
         }),
     );
   }
